d0418480139abe7d4b7aa231a24467f2ea4f072f
Bhupendra Parihar <someone@nowhere.com>
2015-04-11 01:45:34 +0530

    First Commit

---
+++ server.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var request = require("request"),
+	cheerio = require("cheerio"),
+	url = "http://www.eduro.com/";
+	tod = '';
+var app = express();
+
+app.set('port', process.env.PORT || 1337);
+
+
+app.get('/tod', function(req, res) {
+	request(url, function (error, response, body) {
+		if (!error) {
+			var $ = cheerio.load(body),
+				tod = $("dailyquote p").html();
+				console.log(tod); 
+				
+		} else {
+			console.log("We’ve encountered an error: " + error);
+		}
+		res.send(200, {"tod": tod});		
+	});
+});
+
+app.listen(3000);
