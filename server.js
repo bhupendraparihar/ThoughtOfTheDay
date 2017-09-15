658ea4186030488fbe66cf18c989099f088142ae
Bhupendra Parihar <bhupendraparihar@outlook.com>
2017-09-15 11:53:53 +0530

    Remove port unwanted line

--- server.js
+++ server.js
@@ -1,25 +1,24 @@
 var express = require('express');
 var request = require("request"),
-	cheerio = require("cheerio"),
-	url = "http://www.eduro.com/";
-	tod = '';
+    cheerio = require("cheerio"),
+    url = "http://www.eduro.com/";
+tod = '';
 var app = express();
 
-app.set('port', process.env.PORT || 1337);
-
-
 app.get('/', function(req, res) {
-	request(url, function (error, response, body) {
-		if (!error) {
-			var $ = cheerio.load(body),
-				tod = $("dailyquote p").html();
-				console.log(tod); 
-				
-		} else {
-			console.log("We’ve encountered an error: " + error);
-		}
-		res.send(200, {"tod": tod});		
-	});
+    request(url, function(error, response, body) {
+        if (!error) {
+            var $ = cheerio.load(body),
+                tod = $("dailyquote p").html();
+            console.log(tod);
+
+        } else {
+            console.log("We’ve encountered an error: " + error);
+        }
+        res.send(200, { "tod": tod });
+    });
 });
 
-app.listen(3000);
+app.listen(3000, function() {
+    console.log('Express server started at PORT : ' + PORT);
+});
