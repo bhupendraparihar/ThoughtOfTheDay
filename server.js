34f9d244413b316fd21f865cd68d3ef710ea0f9b
Bhupendra Parihar <bhupendraparihar@outlook.com>
2017-09-17 01:20:37 +0530

    Support CORS

--- server.js
+++ server.js
@@ -7,6 +7,12 @@ var app = express();
 
 app.set('port', (process.env.PORT || 3000));
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headerss", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.get('/', function(req, res) {
     request(url, function(error, response, body) {
         if (!error) {
